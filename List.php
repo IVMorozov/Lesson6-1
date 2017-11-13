2d9f0f3f885172f110bf2899301a562a1e6fab1a
IVMorozov <33038726+IVMorozov@users.noreply.github.com>
2017-11-14 00:42:53 +0300

    Create List.php

---
+++ List.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+	  <meta charset="utf-8">
+	  <title>Выбор теста</title>
+  	<link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+
+    <section class="bgrnd-main">
+      <nav>
+        <a class="navigation" href="admin.php">Загрузка теста</a>
+        <a class="navigation active" href="list.php">Выбор тестов</a>
+        <a class="navigation" href="test.php">Тестирование</a>
+      </nav>
+      
+        <p class="help-block">Выберете тест</p>
+    </section>
+    <?php
+
+        $dir = (__DIR__  .  DIRECTORY_SEPARATOR  . Tests);
+        $files = scandir($dir);
+    
+     /*echo '<pre>';
+      print_r($files);
+      echo PHP_EOL;*/
+
+      foreach ($files as $index => $filetype) {
+        If  (strrpos($filetype, 'json')) {$Testlist[]= $filetype;}
+        }    
+      /*echo '<pre>';
+      print_r($Testlist);
+      echo PHP_EOL;*/
+    ?>
+
+      <section class="bgrnd-test">
+      <div class="testarea">
+        <form class="list" action="test.php" method="GET" >
+          <?php            
+            foreach ($Testlist as $index => $file) {
+              echo '<input class="item" type="radio" name="item" value='.$file.'>'.$file.'<Br>';}
+          ?>
+          <p><input class="testchoice" type="submit" value="Выбрать тест"></p>
+        </form>
+      </div>
+  </section>
+  </body>
+</html>
